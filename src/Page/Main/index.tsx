f153f347b18fc7769848fe7bad4e322a835f24d0
gywns022727 <lifford050227a@gmail.com>
2024-02-16 00:19:54 +0900

    update main title

--- src/Page/Main/index.tsx
+++ src/Page/Main/index.tsx
@@ -1,10 +1,10 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function index() {
   return (
     <div>
-      <h1>자퇴신청서</h1>
-      <Link to={"/school"}>자퇴하기!</Link>
+      <h1>자퇴 신청서</h1>
+      <Link to={"/school"}>자퇴 하기!</Link>
     </div>
-  )
+  );
 }
